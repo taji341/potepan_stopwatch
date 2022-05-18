01c40d9a8165957ab903fe2c8db1059e75414084
EC2 Default User <ec2-user@ip-172-31-45-30.ap-northeast-1.compute.internal>
2022-05-18 13:24:56 +0000

    改行、インデント

--- main.js
+++ main.js
@@ -5,38 +5,34 @@ $(function(){
         $3 = $('#3').text(),
         $4 = $('#4').text(),
         timer = 0; 
-    
-    
     $('#start').click(function(){
         $('#start').addClass('disabled');
         $('#stop').removeClass('disabled');
         timer = setInterval(function(){
-            $1++;
-            if($1 == 100) {
-                $1 = 00;
-                $2++;
-                $("#2").text($2);
-            }
-            if($2 == 60) {
-                $2 = 00;
-                $3++;
-                $("#3").text($3);
-            }
-            if($3 == 60) {
-                $2 = 00;
-                $3++;
-                $("#4").text($4);
-            }
-            $("#1").text($1);
-     },10);
-    });
-    
+        $1++;
+        if($1 == 100) {
+            $1 = 00;
+            $2++;
+            $("#2").text($2);
+        }
+        if($2 == 60) {
+            $2 = 00;
+            $3++;
+            $("#3").text($3);
+        }
+        if($3 == 60) {
+            $2 = 00;
+            $3++;
+            $("#4").text($4);
+        }
+        $("#1").text($1);
+         },10);
+        });
     $('#stop').click(function(){
         $('#stop').addClass('disabled');
         $('#start').removeClass('disabled');
         clearInterval(timer);
     });
-    
     $('#reset').click(function(){
         $('#stop').addClass('disabled');
         $('#start').removeClass('disabled');
