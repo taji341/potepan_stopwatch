1dd68f42cbe999a7f3b65e4a714796b184265fb4
EC2 Default User <ec2-user@ip-172-31-45-30.ap-northeast-1.compute.internal>
2022-05-18 05:00:20 +0000

    課題修正

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(function(){
     var $1 = $('#1').text(),
         $2 = $('#2').text(),
         $3 = $('#3').text(),
-        $4 = $('#4').text(),
         timer = 0; 
     
     
@@ -12,20 +11,15 @@ $(function(){
         $('#stop').removeClass('disabled');
         timer = setInterval(function(){
             $1++;
-            if($1 == 10) {
+            if($1 == 60) {
                 $1 = 0;
                 $2++;
                 $("#2").text($2);
             }
-            if($2 == 10) {
+            if($2 == 60) {
                 $2 = 0;
                 $3++;
                 $("#3").text($3);
-            }
-            if($3 == 10) {
-                $3 = 0;
-                $4++;
-                $("#4").text($4);
             }
             $("#1").text($1);
      },1000);
@@ -41,10 +35,9 @@ $(function(){
         $('#stop').addClass('disabled');
         $('#start').removeClass('disabled');
         clearInterval(timer);
-        $1 = $2 = $3 = $4 = 0;
-        $('#1').text(0);
-        $('#2').text(0);
-        $('#3').text(0);
-        $('#4').text(0);
+        $1 = $2 = $3 = $4 = 00;
+        $('#1').text('00');
+        $('#2').text('00');
+        $('#3').text('00');
     });
 })
